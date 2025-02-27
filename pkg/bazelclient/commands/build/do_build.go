f9d414d494f441cea1f7ad0d232871e443663f4a
Ed Schouten <ed@nuxi.nl>
2025-02-27 16:39:20 +0100

    Properly show build error messages

--- pkg/bazelclient/commands/build/do_build.go
+++ pkg/bazelclient/commands/build/do_build.go
@@ -43,6 +43,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -487,6 +488,9 @@ func DoBuild(args *arguments.BuildCommand, workspacePath path.Parser) {
 	}
 	if errBuild != nil {
 		logger.Fatal("Failed to perform build: ", errBuild)
+	}
+	if err := status.FromProto(result.Status); err != nil {
+		logger.Fatal("Failed to perform build: ", err)
 	}
 	logger.Info(result)
 }
