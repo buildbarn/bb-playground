a9f0c3e0df8d457af51a9aa57d7fe1c028467e54
Ed Schouten <eschouten@apple.com>
2025-03-15 11:12:49 +0100

    Add a test for matching multiple file extensions

--- pkg/glob/compile_test.go
+++ pkg/glob/compile_test.go
@@ -51,12 +51,12 @@ func TestCompile(t *testing.T) {
 			nfa, err := glob.Compile([]string{"hello"}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x90, 'h',
-				0x90, 'e',
-				0x90, 'l',
-				0x90, 'l',
-				0x90, 'o',
-				0x81,
+				/*       */ 0x90, 'h',
+				/* h     */ 0x90, 'e',
+				/* he    */ 0x90, 'l',
+				/* hel   */ 0x90, 'l',
+				/* hell  */ 0x90, 'o',
+				/* hello */ 0x81,
 			}, nfa)
 		})
 
@@ -64,8 +64,8 @@ func TestCompile(t *testing.T) {
 			nfa, err := glob.Compile([]string{"*"}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x84,
-				0x81,
+				/*   */ 0x84,
+				/* * */ 0x81,
 			}, nfa)
 		})
 
@@ -73,14 +73,14 @@ func TestCompile(t *testing.T) {
 			nfa, err := glob.Compile([]string{"*a*b*c*"}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x84, // "*".
-				0x90, 'a',
-				0x84, // "*a*".
-				0x90, 'b',
-				0x84, // "*a*b*".
-				0x90, 'c',
-				0x84, // "*a*b*c*".
-				0x81,
+				/*         */ 0x84,
+				/* ꘎       */ 0x90, 'a',
+				/* ꘎a      */ 0x84,
+				/* ꘎a꘎     */ 0x90, 'b',
+				/* ꘎a꘎b    */ 0x84,
+				/* ꘎a꘎b꘎   */ 0x90, 'c',
+				/* ꘎a꘎b꘎c  */ 0x84,
+				/* ꘎a꘎b꘎c꘎ */ 0x81,
 			}, nfa)
 		})
 
@@ -88,14 +88,14 @@ func TestCompile(t *testing.T) {
 			nfa, err := glob.Compile([]string{"a/**/b/**/c"}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x90, 'a',
-				0x90, '/',
-				0x88, // "a/**/".
-				0x90, 'b',
-				0x90, '/',
-				0x88, // "a/**/b/**/".
-				0x90, 'c',
-				0x81,
+				/*             */ 0x90, 'a',
+				/* a           */ 0x90, '/',
+				/* a/          */ 0x88,
+				/* a/꘎꘎/       */ 0x90, 'b',
+				/* a/꘎꘎/b      */ 0x90, '/',
+				/* a/꘎꘎/b/     */ 0x88,
+				/* a/꘎꘎/b/꘎꘎/  */ 0x90, 'c',
+				/* a/꘎꘎/b/꘎꘎/c */ 0x81,
 			}, nfa)
 		})
 
@@ -107,9 +107,9 @@ func TestCompile(t *testing.T) {
 			nfa, err := glob.Compile([]string{"**"}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x88, // "**/".
-				0x84, // "**/*".
-				0x81,
+				/*      */ 0x88,
+				/* ꘎꘎/  */ 0x84,
+				/* ꘎꘎/꘎ */ 0x81,
 			}, nfa)
 		})
 
@@ -120,15 +120,15 @@ func TestCompile(t *testing.T) {
 			nfa, err := glob.Compile([]string{"hello/**"}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x90, 'h',
-				0x90, 'e',
-				0x90, 'l',
-				0x90, 'l',
-				0x90, 'o',
-				0x91, '/',
-				0x88, // "hello/**/".
-				0x84, // "hello/**/*".
-				0x81,
+				/*            */ 0x90, 'h',
+				/* h          */ 0x90, 'e',
+				/* he         */ 0x90, 'l',
+				/* hel        */ 0x90, 'l',
+				/* hell       */ 0x90, 'o',
+				/* hello      */ 0x91, '/',
+				/* hello/     */ 0x88,
+				/* hello/꘎꘎/  */ 0x84,
+				/* hello/꘎꘎/꘎ */ 0x81,
 			}, nfa)
 		})
 
@@ -139,41 +139,41 @@ func TestCompile(t *testing.T) {
 			}, nil)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x90, 'g',
-				0x90, 'o',
-				0x90, 'o',
-				0x90, 'd',
-				0x91, 'b',
-				0x90, 'y',
-				0x90, 'e',
-				0x81,
+				/*         */ 0x90, 'g',
+				/* g       */ 0x90, 'o',
+				/* go      */ 0x90, 'o',
+				/* goo     */ 0x90, 'd',
+				/* good    */ 0x91, 'b',
+				/* goodb   */ 0x90, 'y',
+				/* goodby  */ 0x90, 'e',
+				/* goodbye */ 0x81,
 			}, nfa)
 		})
 
-		t.Run("Branch 1", func(t *testing.T) {
+		t.Run("Branch1", func(t *testing.T) {
 			nfa, err := glob.Compile(
 				[]string{"goodbye"},
 				[]string{"good work"},
 			)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x90, 'g',
-				0x90, 'o',
-				0x90, 'o',
-				0x90, 'd',
-				0xa0, ' ', 'b',
-				0x90, 'w',
-				0x90, 'y',
-				0x90, 'o',
-				0x90, 'e',
-				0x90, 'r',
-				0x81, // End of "goodbye".
-				0x90, 'k',
-				0x82, // End of "good work".
+				/*           */ 0x90, 'g',
+				/* g         */ 0x90, 'o',
+				/* go        */ 0x90, 'o',
+				/* goo       */ 0x90, 'd',
+				/* good      */ 0xa0, ' ', 'b',
+				/* good␣     */ 0x90, 'w',
+				/* goodb     */ 0x90, 'y',
+				/* good␣w    */ 0x90, 'o',
+				/* goodby    */ 0x90, 'e',
+				/* good␣wo   */ 0x90, 'r',
+				/* goodbye   */ 0x81,
+				/* good␣wor  */ 0x90, 'k',
+				/* good␣work */ 0x82,
 			}, nfa)
 		})
 
-		t.Run("Branch 2", func(t *testing.T) {
+		t.Run("Branch2", func(t *testing.T) {
 			// Swapping the strings around should not affect
 			// the order in which states are returned.
 			nfa, err := glob.Compile(
@@ -182,19 +182,66 @@ func TestCompile(t *testing.T) {
 			)
 			require.NoError(t, err)
 			require.Equal(t, []byte{
-				0x90, 'g',
-				0x90, 'o',
-				0x90, 'o',
-				0x90, 'd',
-				0xa0, ' ', 'b',
-				0x90, 'w',
-				0x90, 'y',
-				0x90, 'o',
-				0x90, 'e',
-				0x90, 'r',
-				0x82, // End of "goodbye".
-				0x90, 'k',
-				0x81, // End of "good work".
+				/*           */ 0x90, 'g',
+				/* g         */ 0x90, 'o',
+				/* go        */ 0x90, 'o',
+				/* goo       */ 0x90, 'd',
+				/* good      */ 0xa0, ' ', 'b',
+				/* good␣     */ 0x90, 'w',
+				/* goodb     */ 0x90, 'y',
+				/* good␣w    */ 0x90, 'o',
+				/* goodby    */ 0x90, 'e',
+				/* good␣wo   */ 0x90, 'r',
+				/* goodbye   */ 0x82,
+				/* good␣wor  */ 0x90, 'k',
+				/* good␣work */ 0x81,
+			}, nfa)
+		})
+
+		t.Run("FileExtensions", func(t *testing.T) {
+			nfa, err := glob.Compile(
+				[]string{
+					"*.h",
+					"*.H",
+					"*.h++",
+					"*.hh",
+					"*.hpp",
+					"*.hxx",
+					"*.inc",
+					"*.inl",
+					"*.ipp",
+					"*.tcc",
+					"*.tlh",
+					"*.tli",
+				},
+				nil,
+			)
+			require.NoError(t, err)
+			require.Equal(t, []byte{
+				/*       */ 0x84,
+				/* ꘎     */ 0x90, '.',
+				/* ꘎.    */ 0x40, 0x40, 'H', 'h', 'i', 't',
+				/* ꘎.H   */ 0x81,
+				/* ꘎.h   */ 0x40, 0x41, '+', 'h', 'p', 'x',
+				/* ꘎.i   */ 0xa0, 'n', 'p',
+				/* ꘎.t   */ 0xa0, 'c', 'l',
+				/* ꘎.h+  */ 0x90, '+',
+				/* ꘎.hh  */ 0x81,
+				/* ꘎.hp  */ 0x90, 'p',
+				/* ꘎.hx  */ 0x90, 'x',
+				/* ꘎.in  */ 0xa0, 'c', 'l',
+				/* ꘎.ip  */ 0x90, 'p',
+				/* ꘎.tc  */ 0x90, 'c',
+				/* ꘎.tl  */ 0xa0, 'h', 'i',
+				/* ꘎.h++ */ 0x81,
+				/* ꘎.hpp */ 0x81,
+				/* ꘎.hxx */ 0x81,
+				/* ꘎.inc */ 0x81,
+				/* ꘎.inl */ 0x81,
+				/* ꘎.ipp */ 0x81,
+				/* ꘎.tcc */ 0x81,
+				/* ꘎.tlh */ 0x81,
+				/* ꘎.tli */ 0x81,
 			}, nfa)
 		})
 	})
